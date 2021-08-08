a86f4e3b221ffff93ec083de78b9d11b94ad0a95
Priyanka-23 <priyankatiwary23@gmail.com>
2021-08-08 11:02:32 +0530

    adding modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 import './App.css';
 import { ModalDisplay } from './ModalDisplay';
 
@@ -31,16 +32,13 @@ function App() {
         const status=()=>{
           if(launch.upcoming===true) return <p className="upcoming"> Upcoming</p>;
           else if(launch.launch_success===true) return <p className="success"> Success </p>;
-          else if(launch.launch_success===false) return <p className="fail">Failed</p>;
+          else if(launch.launch_success!==true) return <p className="fail">Failed</p>;
         };
-        
-      let date=launch.launch_date_local;
-      // let all=false;
-          return <tr key={key} className="tableRow" onClick={()=>{setShow(true)}} >
+          return <tr key={key} className="tableRow" onClick={()=>{setShow(true); console.log(show)}} >
           { mystatus==='all'? 
             <>
             <td>{launch.flight_number}</td>
-            <td>{date}</td>
+            <td>{launch.launch_date_utc}</td>
             <td>{launch.launch_site.site_name}</td>
             <td>{launch.mission_name}</td>
             <td>{status()}</td>
@@ -48,29 +46,40 @@ function App() {
           launch.launch_success===mystatus &&
             <>
             <td>{launch.flight_number}</td>
-            <td>{date}</td>
+            <td>{launch.launch_date_utc}</td>
             <td>{launch.launch_site.site_name}</td>
             <td>{launch.mission_name}</td>
             <td>{status()}</td>
             </>
           }
-          
-           </tr>
-           
+           </tr>   
       })
     )
-  }
+  }  
   return (
     <>
     <h1 className="heading">SpaceX</h1>
-    <div className="status">
+    {/* <Dropdown class="status">
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    All
+  </Dropdown.Toggle> */}
+
+  <DropdownButton className="filter"title="All">
+    <Dropdown.Item className="filterItem-all" onclick={(()=>{setStatus('all')})}>All</Dropdown.Item><br/>
+    <Dropdown.Item className="filterItem-upcoming" onClick={(()=>{setStatus(null)})}>Upcoming</Dropdown.Item><br/>
+    <Dropdown.Item className="filterItem-success" onClick={(()=>{setStatus(true)})} >Success</Dropdown.Item><br/>
+    <Dropdown.Item className="filterItem-failure" onClick={(()=>{setStatus(false)})}>Failure</Dropdown.Item>
+  </DropdownButton>
+{/* </Dropdown> */}
+    {/* <div className="status">
     <button className="upcoming" onClick={(()=>{setStatus(null);})}>Upcoming</button>
     <button className="success" onClick={(()=>{setStatus(true);})}>Success</button>
     <button className="fail" onClick={(()=>{setStatus(false);})}>Failure</button>
     <button className="All" onClick={(()=>{setStatus('all');})}>All</button>
-    </div>
-   { launches.map((launch,key)=>{
-     return(
+    </div> */}
+   
+   { launches.map((launch,key)=>{ 
+      return (
     <ModalDisplay 
       show={show} 
       onhide={()=>setShow(false)} 
@@ -82,11 +91,10 @@ function App() {
       rocket_name={launch.rocket.rocket_name}
       manufacturer='SpaceX'
       nationality='SpaceX'
-      centered/>
-     )
-    })
-   
-  }
+      />
+      )
+    })}
+
       <table className="main">
       <thead>
         {tableHeader()}
--- src/ModalDisplay.js
+++ src/ModalDisplay.js
@@ -1,65 +1,72 @@
 import React from 'react';
-import Modal from 'react-bootstrap/Modal';
 import { Button, Col, Container, Row } from 'react-bootstrap';
-export function ModalDisplay(props) {
-  console.log(props);
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <img src={props.img}/>
-            <h4>{props.mission}</h4>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Container>
-          <Row>
-         <Col> {props.details} </Col>
-          </Row>
-          <Row>
-            <Col>Flight Number</Col>
-            <Col xs={9}>{props.flight_number}</Col>
-          </Row>
-          <Row>
-            <Col>Mission Name</Col>
-            <Col xs={9}>{props.mission_name}</Col>
-          </Row>
-          <Row>
-            <Col>Rocket Type</Col>
-            <Col xs={9}>{props.rocket_type}</Col>
-          </Row>
-          <Row>
-            <Col>Rocket Name</Col>
-            <Col xs={9}>{props.rocket_name}</Col>
-          </Row>
-          <Row>
-            <Col>Manufacturer</Col>
-            <Col xs={9}>{props.manufacturer}</Col>
-          </Row>
-          <Row>
-            <Col>Nationality</Col>
-            <Col xs={9}>{props.nationality}</Col>
-          </Row>
-          {/* <Row>
-            <Col>Payload Type</Col>
-            <Col xs={9}>{props.payload_type}</Col>
-          </Row> */}
+import {Modal} from 'react-bootstrap';
+
+ const MODAL_STYLES={
+   position:'fixed',
+   top:'50%',
+   left:'50%',
+   transform:'translate(-50%, -50%)',
+   padding:'50px',
+   backgroundColor:'grey',
+   zIndex:1000
+ }
+ const IMG_STYLES={
+   width:'10%',
+   height:'10%',
+ }
+ export function ModalDisplay(props){
+  return(
+  <Modal style={MODAL_STYLES}
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+        <Container>
+            <Row>
+              <Col><img style={IMG_STYLES} src={props.img}/></Col>
+              <Col><h3>{props.mission}</h3></Col>
+              <Col><button onClick={props.onHide}>X</button></Col>
+            </Row>
+            </Container>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      <Container>
+            <Row>
+              <Col><h4>Flight Number</h4></Col>
+              <Col>{props.flight_number}</Col>
+            </Row>
+            <Row>
+              <Col><h4>Mission Name</h4></Col>
+              <Col>{props.mission_name}</Col>
+            </Row>
+            <Row>
+              <Col><h4>Rocket Type</h4></Col>
+              <Col>{props.rocket_type}</Col>
+            </Row>
+            <Row>
+              <Col><h4>Rocket Name</h4></Col>
+              <Col>{props.rocket_name}</Col>
+            </Row>
+            <Row>
+              <Col><h4>Manufacturer</h4></Col>
+              <Col>{props.manufacturer}</Col>
+            </Row>
+            <Row>
+              <Col><h4>Nationality</h4></Col>
+              <Col>{props.nationality}</Col>
+            </Row>
           </Container>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+          
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   
-  
-  
-  // render(<ModalDisplay />);
-  
+  )
+}
